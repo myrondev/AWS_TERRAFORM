dd2d7f8e9138f2dc5f5af2f2efb385c35867e488
myrondev <mzaiets@sorint.it>
2022-01-12 20:18:40 +0100

    added new files

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,13 @@
 # resource "aws_instance" "my_server" {
 #   ami           = ""
-#   instance_type = "t2.micro"
+#   instance_type = var.instance_defined_type
 
 #   tags = {
 #     Name = "example_test"
 #   }
+# }
+
+
+# variable "instance_defined_type" {
+#   type = string
 # }
--- vpc.tf
+++ vpc.tf
@@ -3,6 +3,10 @@ module "vpc" {
   version = "3.11.0"
   # insert the 21 required variables here
 
-  name = "my-new-vpc"
+  name = "my-new-${local.vpc_name}"
   cidr = "10.0.0.0/16"
+}
+
+locals {
+  vpc_name = "test"
 }
